588fab9cdd89458b06be98994c88e37fd5432c64
nikechukwu <nikechukwu@googlemail.com>
2025-05-14 16:18:20 +0000

    added deprecate and update command

---
+++ cli/lib/src/cmd/deprecate.dart
@@ -0,0 +1,18 @@
+
+
+import 'package:pritt_cli/src/cli/base.dart';
+
+class DeprecateCommand extends PrittCommand {
+  @override
+  String name = "deprecate";
+
+  @override
+  String description = "Deprecate a given package";
+
+  @override
+  void run() {
+    // TODO: implement run
+    throw UnimplementedError();
+  }
+
+}
---
+++ cli/lib/src/cmd/update.dart
@@ -0,0 +1,15 @@
+
+
+import 'package:args/command_runner.dart';
+
+class UpdateCommand extends Command {
+  @override
+  String name = "update";
+
+  @override
+  String description = "Updates the Pritt CLI";
+
+  UpdateCommand() {
+    argParser.addFlag('auto', negatable: false, help: "Configure Pritt to auto-update whenever you log into shell");
+  }
+}
