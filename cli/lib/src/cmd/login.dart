521e797bbb0b04057a90c3a0130ed749fc75bdae
Nike Okoronkwo <nikechukwu@gmail.com>
2025-05-17 10:42:46 -0400

    wip: cli integration

--- cli/lib/src/cmd/login.dart
+++ cli/lib/src/cmd/login.dart
@@ -1,9 +1,11 @@
 import 'dart:async';
 
 import 'package:args/command_runner.dart';
+import 'package:pritt_cli/src/client.dart';
 import 'package:pritt_cli/src/constants.dart';
 import 'package:pritt_cli/src/user_config.dart';
 import 'package:pritt_cli/src/utils/extensions.dart';
+import 'package:pritt_cli/src/utils/run.dart';
 
 import '../cli/base.dart';
 
@@ -39,19 +41,43 @@ class LoginCommand extends PrittCommand {
       } else if (!url.isUrl) {
         throw UsageException("'url' option must be valid URL", usage);
       }
+    } else {
+      url = mainPrittInstance;
     }
+
+    final client = PrittClient(url: url);
 
     // check if user is logged in
     var userCredentials = await UserCredentials.fetch();
 
-    if (userCredentials != null) {
+    if (userCredentials == null) {
       // else log user in
+      userCredentials = await loginUser(client, rootRunner);
     } else {
+      if (userCredentials.accessTokenExpires.isBefore(DateTime.now())) {
+        // if user is logged in, but token is expired, log user in
+        logger.info('Access token expired. Logging in again...');
+        // log user in
+        userCredentials = await loginUser(client, rootRunner);
+      } else {
+        // else
+        // if user is logged in, and token is not expired, display user info
+        logger.info("You are already logged in...");
+      }
       // if user is logged in, state that user is logged in
     }
+
+    await userCredentials.update();
 
     // get user info from login details
 
     // display user log in info
   }
+}
+
+/// Log a user in to the Pritt server
+Future<UserCredentials> loginUser(PrittClient client, Runner cmdRunner, {UserCredentials? credentials}) async{
+  // request for an auth 
+
+  throw UnimplementedError('Login not implemented yet');
 }
--- cli/lib/src/constants.dart
+++ cli/lib/src/constants.dart
@@ -1,3 +1,6 @@
 const mainPrittInstance =
     String.fromEnvironment('PRITT_URL', defaultValue: defaultPrittInstance);
 const defaultPrittInstance = 'https://pritt.dev/';
+
+final mainPrittUrl = Uri.parse(mainPrittInstance);
+final mainPrittApiUrl = mainPrittUrl.replace(host: 'api.${mainPrittUrl.host}');
