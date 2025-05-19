caab6f8a5c08e4dbe0ba2c846a20f8b4651e81cb
Nike Okoronkwo <nikechukwu@gmail.com>
2025-05-18 23:48:47 -0400

    added e2e

--- packages/openapigen/js/src/main.ts
+++ packages/openapigen/js/src/main.ts
@@ -1,132 +1,192 @@
-import { dereference, load } from "@scalar/openapi-parser";
-import { OpenAPIV3 } from "@scalar/openapi-types";
+import { dereference, load } from '@scalar/openapi-parser';
+import { OpenAPIV3 } from '@scalar/openapi-types';
 import { isEqual } from 'lodash';
 
 interface OpenAPIGenResult {
-    docs?: string;
-    schemas: Record<string, OpenAPIV3.SchemaObject>;
-    methods: OAPIGenMethods[];
+  docs?: string;
+  schemas: Record<string, OpenAPIV3.SchemaObject>;
+  methods: OAPIGenMethods[];
 }
 
 interface OAPIGenMethods {
-    name?: string; //
-    path: string; //
-    method: string; //
-    summary?: string; //
-    description?: string; //
-    parameters: OpenAPIV3.ParameterObject[];
-    body?: {
-        type: string,
-        name?: string,
-        schema?: OpenAPIV3.SchemaObject
-    } | {
-        type: undefined,
-        name: undefined,
-        schema: undefined,
-        ref?: string,
-    };
-    returns?: Record<string, {
-        type: string,
-        name?: string,
-        schema?: OpenAPIV3.SchemaObject
-    }>;
+  name?: string; //
+  path: string; //
+  method: string; //
+  summary?: string; //
+  description?: string; //
+  parameters: OpenAPIV3.ParameterObject[];
+  body?:
+    | {
+        type: string;
+        name?: string;
+        schema?: OpenAPIV3.SchemaObject;
+      }
+    | {
+        type: undefined;
+        name: undefined;
+        schema: undefined;
+        ref?: string;
+      };
+  returns?: Record<
+    string,
+    {
+      type: string;
+      name?: string;
+      schema?: OpenAPIV3.SchemaObject;
+    }
+  >;
 }
-
 
 /** Parses the OpenAPI document and generates an object map used for generating code */
 export async function parseOpenAPIDocumentSource(source: string) {
-    // dereference
-    const { schema } = await dereference(source);
-    if (!schema) throw Error("Schema did not parse");
+  // dereference
+  const { schema } = await dereference(source);
+  if (!schema) throw Error('Schema did not parse');
 
-    // return openapiDocToResult(schema as OpenAPIV3.Document);
-    return openapiDocToResult(schema as OpenAPIV3.Document);
+  // return openapiDocToResult(schema as OpenAPIV3.Document);
+  return openapiDocToResult(schema as OpenAPIV3.Document);
 }
 
 function openapiDocToResult(spec: OpenAPIV3.Document): OpenAPIGenResult {
-    const schemas = spec.components?.schemas ?? {}
+  const schemas = spec.components?.schemas ?? {};
 
-    function searchComponent(schema: object) : string | undefined {
-        return Object.entries(schemas).find(([k, v]) => isEqual(v, schema))?.[0];
-    }
+  function searchComponent(schema: object): string | undefined {
+    return Object.entries(schemas).find(([k, v]) => isEqual(v, schema))?.[0];
+  }
 
-    function transformBodySchema(responseInfo: OpenAPIV3.ReferenceObject | OpenAPIV3.RequestBodyObject | undefined): {
-        type: string,
-        name?: string,
-        schema?: OpenAPIV3.SchemaObject,
-        required?: boolean,
-    } | {
-        type: undefined,
-        name: undefined,
-        schema: undefined,
-        ref?: string,
-        required?: boolean,
-    } | undefined {
-        if (responseInfo) {
-            if ('$ref' in responseInfo) {
-                return {
-                    type: undefined,
-                    name: undefined,
-                    schema: undefined,
-                    ref: responseInfo.$ref
-                }
-            } else if (responseInfo?.content) {
-                const [[contentType, obj]] = Object.entries(responseInfo.content as {
-                    [media: string]: OpenAPIV3.MediaTypeObject;
-                })
-
-                let name = obj.schema?.title ? (obj.schema.title !== 'schema' ? obj.schema?.title : 'unknown') : undefined;
-                if (name === 'unknown') name = searchComponent(obj);
-
-                return {
-                    type: contentType,
-                    name,
-                    schema: obj,
-                    required: responseInfo.required
-                }
-            }
-        }
-    }
-
-    function generateMethod(operation: OpenAPIV3.OperationObject | undefined, name: string, path: string, parameters: OpenAPIV3.ParameterObject[] = []) : OAPIGenMethods {
+  function transformBodySchema(
+    responseInfo:
+      | OpenAPIV3.ReferenceObject
+      | OpenAPIV3.RequestBodyObject
+      | undefined
+  ):
+    | {
+        type: string;
+        name?: string;
+        schema?: OpenAPIV3.SchemaObject;
+        required?: boolean;
+      }
+    | {
+        type: undefined;
+        name: undefined;
+        schema: undefined;
+        ref?: string;
+        required?: boolean;
+      }
+    | undefined {
+    if (responseInfo) {
+      if ('$ref' in responseInfo) {
         return {
-            method: name,
-            path: path,
-            name: operation?.operationId,
-            summary: operation?.summary,
-            description: operation?.description,
-            parameters: [...[...(operation?.parameters as OpenAPIV3.ParameterObject[] ?? []), ...(parameters)]],
-            body: transformBodySchema(operation?.requestBody),
-            returns: Object.assign(
-                {},
-                ...Object.entries(operation?.responses ?? {}).map(([code, responseInfo]) => {
-                    const body = transformBodySchema(responseInfo);
-                    return {
-                        [code]: body
-                    }
-                })
-            )
-        }
-    }
+          type: undefined,
+          name: undefined,
+          schema: undefined,
+          ref: responseInfo.$ref,
+        };
+      } else if (responseInfo?.content) {
+        const [[contentType, obj]] = Object.entries(
+          responseInfo.content as {
+            [media: string]: OpenAPIV3.MediaTypeObject;
+          }
+        );
 
+        let name = obj.schema?.title
+          ? obj.schema.title !== 'schema'
+            ? obj.schema?.title
+            : 'unknown'
+          : undefined;
+        if (name === 'unknown') name = searchComponent(obj);
+
+        return {
+          type: contentType,
+          name,
+          schema: obj,
+          required: responseInfo.required,
+        };
+      }
+    }
+  }
+
+  function generateMethod(
+    operation: OpenAPIV3.OperationObject | undefined,
+    name: string,
+    path: string,
+    parameters: OpenAPIV3.ParameterObject[] = []
+  ): OAPIGenMethods {
     return {
-        docs: spec.info?.description,
-        schemas,
-        methods: Object.entries(spec.paths ?? {}).map(([k, v]) => {
-            const returnArray: OAPIGenMethods[] = [];
-            if (v?.get) {
-                returnArray.push(generateMethod(v.get, 'GET', k, v.parameters as OpenAPIV3.ParameterObject[]));
-            }
-            if (v?.post) {
-                returnArray.push(generateMethod(v.post, 'POST', k, v.parameters as OpenAPIV3.ParameterObject[]));
-            }
-            if (v?.delete) {
-                returnArray.push(generateMethod(v.delete, 'DELETE', k, v.parameters as OpenAPIV3.ParameterObject[]));
-            }
-            if (v?.put) {
-                returnArray.push(generateMethod(v.put, 'PUT', k, v.parameters as OpenAPIV3.ParameterObject[]));
-            }
-            return returnArray;
-        }).reduce((previous, current, index) => previous.concat(current))
+      method: name,
+      path: path,
+      name: operation?.operationId,
+      summary: operation?.summary,
+      description: operation?.description,
+      parameters: [
+        ...[
+          ...((operation?.parameters as OpenAPIV3.ParameterObject[]) ?? []),
+          ...parameters,
+        ],
+      ],
+      body: transformBodySchema(operation?.requestBody),
+      returns: Object.assign(
+        {},
+        ...Object.entries(operation?.responses ?? {}).map(
+          ([code, responseInfo]) => {
+            const body = transformBodySchema(responseInfo);
+            return {
+              [code]: body,
+            };
+          }
+        )
+      ),
     };
+  }
+
+  return {
+    docs: spec.info?.description,
+    schemas,
+    methods: Object.entries(spec.paths ?? {})
+      .map(([k, v]) => {
+        const returnArray: OAPIGenMethods[] = [];
+        if (v?.get) {
+          returnArray.push(
+            generateMethod(
+              v.get,
+              'GET',
+              k,
+              v.parameters as OpenAPIV3.ParameterObject[]
+            )
+          );
+        }
+        if (v?.post) {
+          returnArray.push(
+            generateMethod(
+              v.post,
+              'POST',
+              k,
+              v.parameters as OpenAPIV3.ParameterObject[]
+            )
+          );
+        }
+        if (v?.delete) {
+          returnArray.push(
+            generateMethod(
+              v.delete,
+              'DELETE',
+              k,
+              v.parameters as OpenAPIV3.ParameterObject[]
+            )
+          );
+        }
+        if (v?.put) {
+          returnArray.push(
+            generateMethod(
+              v.put,
+              'PUT',
+              k,
+              v.parameters as OpenAPIV3.ParameterObject[]
+            )
+          );
+        }
+        return returnArray;
+      })
+      .reduce((previous, current, index) => previous.concat(current)),
+  };
 }
--- packages/openapigen/js/test/openapi.test.ts
+++ packages/openapigen/js/test/openapi.test.ts
@@ -1,21 +1,21 @@
-import { test } from "vitest";
-import { readdirSync, readFileSync } from "node:fs";
-import { parseOpenAPIDocumentSource } from "../src/main.js";
-import { join } from "node:path";
-import { dereference } from "@scalar/openapi-parser";
-import { OpenAPIV3 } from "@scalar/openapi-types";
+import { test } from 'vitest';
+import { readdirSync, readFileSync } from 'node:fs';
+import { parseOpenAPIDocumentSource } from '../src/main.js';
+import { join } from 'node:path';
+import { dereference } from '@scalar/openapi-parser';
+import { OpenAPIV3 } from '@scalar/openapi-types';
 
 const specDir = './test/specs';
 
 test('', async () => {
-    const dir = readdirSync(specDir, { encoding: 'utf8' });
-    for (const specFile of dir) {
-        const file = readFileSync(join(specDir, specFile), { encoding: 'utf8' });
-        // console.log(file);
-        const {schema} = await dereference(file);
-        const spec = schema as OpenAPIV3.Document;
+  const dir = readdirSync(specDir, { encoding: 'utf8' });
+  for (const specFile of dir) {
+    const file = readFileSync(join(specDir, specFile), { encoding: 'utf8' });
+    // console.log(file);
+    const { schema } = await dereference(file);
+    const spec = schema as OpenAPIV3.Document;
 
-        const exportableSpec = await parseOpenAPIDocumentSource(file);
-        console.log(spec.paths);
-    }
+    const exportableSpec = await parseOpenAPIDocumentSource(file);
+    console.log(spec.paths);
+  }
 });
