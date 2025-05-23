6fed6baf5b541dbde63cea016ccd26ccb1caf854
Nike Okoronkwo <nikechukwu@gmail.com>
2025-05-23 00:34:57 -0400

    formatting and wip: cas impl

--- server/lib/src/main/cas/client.dart
+++ server/lib/src/main/cas/client.dart
@@ -2,3 +2,27 @@
 abstract class CASClient {
   
 }
+
+class CASRequest {
+  final String id;
+  final String method;
+  final Map<String, dynamic> params;
+
+  const CASRequest({
+    required this.id,
+    required this.method,
+    required this.params
+  });
+}
+
+class CASResponse<T> {
+  final String id;
+  final T? data;
+  final String error;
+
+  const CASResponse({
+    required this.id,
+    required this.data,
+    required this.error
+  });
+}
