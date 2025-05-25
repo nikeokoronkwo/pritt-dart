f7b668f98ebc4282a8e4ecf41b5ec8655d4b7fd8
Nike Okoronkwo <nikechukwu@gmail.com>
2025-05-25 16:41:29 -0400

    updated actions and workflows

--- cli/tool/grind.dart
+++ cli/tool/grind.dart
@@ -1,1 +1,22 @@
+import 'dart:io';
 
+import 'package:grinder/grinder.dart';
+import 'package:cli_pkg/cli_pkg.dart' as pkg;
+
+void main(List<String> args) {
+  // values
+  pkg.humanName.value = "Pritt";
+  pkg.executables.value = {"pritt": "bin/pritt.dart"};
+
+
+  pkg.githubUser.fn = () => Platform.environment["GH_USER"];
+  pkg.githubPassword.fn = () => Platform.environment["GH_TOKEN"];
+
+
+  // add tasks and grind
+  pkg.addAllTasks();
+  grind(args);
+}
+
+@DefaultTask("Compile Code and Format")
+void all() {}
