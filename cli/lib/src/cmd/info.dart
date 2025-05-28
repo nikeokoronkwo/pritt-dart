8fa896b7d8f7ac2ed40a11167fe91480e97ffe86
Nike Okoronkwo <nikechukwu@gmail.com>
2025-05-28 14:07:03 -0400

    CLI: Implement `info`

--- cli/lib/src/cmd/info.dart
+++ cli/lib/src/cmd/info.dart
@@ -1,4 +1,10 @@
 import 'dart:async';
+import 'dart:io';
+import 'dart:math';
+
+import 'package:io/ansi.dart';
+import 'package:pritt_cli/src/client.dart';
+import 'package:pritt_cli/src/user_config.dart';
 
 import '../cli/base.dart';
 
@@ -11,13 +17,27 @@ class InfoCommand extends PrittCommand {
       "Get current information about the currently logged in user";
 
   @override
-  FutureOr? run() {
+  void run() async {
     // check if user is logged in
+    var userCredentials = await UserCredentials.fetch();
 
-    // if user not logged in, tell user to log in
+    if (userCredentials == null || userCredentials.isExpired) {
+      // if user not logged in, tell user to log in
+      logger.severe(userCredentials == null ? 'You are not logged in to Pritt' : 'Your login session has expired');
+      logger.severe('To log in, run: ${styleBold.wrap('pritt login')}');
+      exit(1);
+    }
 
     // if user is logged in, get user info
+    final client = PrittClient(url: userCredentials.uri.toString(), accessToken: userCredentials.accessToken);
+    final user = await client.getCurrentUser();
 
     // print user info as table
+    logger.fine('User Information For: ${styleDim.wrap('${user.name} <${user.email}>')}');
+    final userAsJson = user.toJson();
+
+    userAsJson.entries.map((e) {
+      logger.stdout('\t${styleBold.wrap(e.key)}: ${e.value}');
+    });
   }
 }
