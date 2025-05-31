ac6be229b5ca956f9470999b2e166c2929b818ba
Nike Okoronkwo <nikechukwu@gmail.com>
2025-05-31 01:40:16 -0400

    WIP: [web] updated auth.vue

--- web/pages/auth.vue
+++ web/pages/auth.vue
@@ -10,7 +10,28 @@ definePageMeta({
     middleware: 'auth'
 })
 
+const route = useRoute();
 
+const session = authClient.useSession()
+
+const state = ref<'pending' | 'error' | 'success'>('pending');
+const error = ref<string | null>(null);
+const errorMessage = ref<string | null>(null);
+
+onMounted(() => {
+    // check the query hash
+    const query = route.query.id;
+    if (!query) {
+        state.value = 'error';
+        error.value = 'Unknown Session'
+        errorMessage.value = 'Unknown session, redirecting soon'
+        setTimeout(() => {
+            return navigateTo('/')
+        }, 2500);
+    }
+
+    // validate user if logged in
+});
 </script>
 
 <template>
@@ -18,7 +39,15 @@ definePageMeta({
         <!-- Auth Platform -->
         <div>
             <!-- Validate if the user is authenticating to the Pritt CLI? -->
-            <div></div>
+            <div v-if="state === 'pending'">
+
+            </div>
+            <div v-else-if="state === 'error'">
+
+            </div>
+            <div v-else>
+
+            </div>
         </div>
     </div>
 </template>
