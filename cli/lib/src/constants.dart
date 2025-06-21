c79e4554d9328c5021c2cb0802d38b5273ca9b77
Nike Okoronkwo <nikechukwu@gmail.com>
2025-06-21 14:38:05 -0400

    Switched `fromEnvironment` (compile-time) to `Platform.environment` (runtime) except for secret-based values

--- cli/lib/src/constants.dart
+++ cli/lib/src/constants.dart
@@ -1,8 +1,8 @@
-const mainPrittInstance =
-    String.fromEnvironment('PRITT_URL', defaultValue: defaultPrittInstance);
+import 'dart:io';
+
+final mainPrittInstance = Platform.environment['PRITT_URL'] ?? defaultPrittInstance;
 const defaultPrittInstance = 'https://pritt.dev/';
 
 final mainPrittUrl = Uri.parse(mainPrittInstance);
 final mainPrittApiUrl = mainPrittUrl.replace(host: 'api.${mainPrittUrl.host}');
-final mainPrittApiInstance = String.fromEnvironment('PRITT_API_URL',
-    defaultValue: mainPrittApiUrl.toString());
+final mainPrittApiInstance = Platform.environment['PRITT_API_URL'] ?? mainPrittApiUrl.toString();
