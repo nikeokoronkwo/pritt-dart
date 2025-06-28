a9d5efcfe58c219e1340c96351aa0690a8618958
Nike Okoronkwo <nikechukwu@gmail.com>
2025-06-27 21:43:17 -0400

    switching to hashicorp providers

--- infra/README.md
+++ infra/README.md
@@ -1,1 +1,2 @@
 # Pritt Infrastructure
+Pritt infra is provisioned using Opentofu, but it is made to be compatible with Terraform where possible
--- infra/main.tf
+++ infra/main.tf
@@ -1,7 +1,7 @@
 terraform {
   required_providers {
     google = {
-      source = "opentofu/google"
+      source = "hashicorp/google"
       version = "6.38.0"
     }
   }
